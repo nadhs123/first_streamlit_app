f598fced94e965148ad2fde29fc58aad94443c46
nadhs123 <130352526+nadhs123@users.noreply.github.com>
2023-04-10 14:21:31 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,6 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Dinner')
-streamlit.title('Breakfast menu','omega 3','boiled eggs')
+streamlit.title('Breakfast menu')
+streamlit.title('omega 3')
+streamlit.title('boiled eggs')
