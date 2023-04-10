11e70f02d81a02df4d9f00ee41e44371bdd8965b
nadhs123 <130352526+nadhs123@users.noreply.github.com>
2023-04-10 14:29:59 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,3 +6,5 @@ streamlit.header('Breakfast Favourites')
 streamlit.text(' 🥣 omega 3 & Blueberry oatmeal')
 streamlit.text(' 🐔 boiled eggs')
 streamlit.text('🥑🍞 Avacado Toast')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
